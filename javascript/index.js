651d85adb1cdc0ff18d6da115fece15f9c2be29a
jureefitz <42006462+jureefitz@users.noreply.github.com>
2023-02-11 02:08:04 -0500

    Add link to all cities

--- javascript/index.js
+++ javascript/index.js
@@ -37,11 +37,13 @@ function updateCity(event) {
      <div>
      <h2>${cityName}</h2>
      <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-        
-     </div>
+    </div>
+
      <div class="time">${cityTime.format("h:mm:ss")} 
        <small>${cityTime.format("A")}</small>
-     </div> 
+     </div>
+     </div>
+     <a href="/">All cities</a>
       `;
      }
 
